9dd6d42f2d62afbadb5be7af0b7238674a7b9082
Brandon Fulljames <bfullj@gmail.com>
2020-06-22 12:26:11 +0900

    Color tweak for player

--- src/game/entities/player.rs
+++ src/game/entities/player.rs
@@ -7,11 +7,11 @@ pub fn create_in(world: &mut World) {
         .with(components::Position { x: 0., y: 0. })
         .with(components::Draw {
             fg_r: 255,
-            fg_g: 128,
+            fg_g: 64,
             fg_b: 128,
             bg_r: 0,
-            bg_g: 128,
-            bg_b: 128,
+            bg_g: 0,
+            bg_b: 0,
             rune: '@',
         })
         .build();
